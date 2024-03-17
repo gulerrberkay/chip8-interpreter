91fd6c52450fd91e990674ee604c54fc549d5b5b
Berkay <berkay.guler1@gmail.com>
2024-03-17 22:07:25 +0100

    fix: include keypad func

--- include/chip8-core.h
+++ include/chip8-core.h
@@ -1,9 +1,11 @@
 #ifndef CHIP8_CORE_H
 #define CHIP8_CORE_H
 
+#include "opcodes.h"
+
 void initialize_chip();
 void load_rom();
 void emulate_cycle();
-void update_key();
+void update_keypad();
 
 #endif
